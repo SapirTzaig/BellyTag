f3c16d8ae75c166746e7d23a43cbce3d1405c268
SapirTzaig <tzaigsap@post.bgu.ac.il>
2025-03-05 10:31:35 +0200

    main update

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from './components/Login.vue';
 import Upload from './components/Upload.vue';
 import Dashboard from './components/Dashboard.vue';
+import PatientScreen from './components/PatientScreen.vue';
 
 const routes = [
   { path: '/', component: Login },
   { path: '/upload', component: Upload },
   { path: '/dashboard', component: Dashboard },
+  { path: '/patient/:id', component: PatientScreen }
 ];
 
 const router = createRouter({
