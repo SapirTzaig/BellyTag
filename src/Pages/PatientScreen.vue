0ea9dd5751a115421c309e7c57052314b7de7759
LiorAmitay1 <lioramit@post.bgu.ac.il>
2025-03-05 13:05:36 +0200

    add pages
    add uder details page, upload file page and changes in patient

--- src/Pages/PatientScreen.vue
+++ src/Pages/PatientScreen.vue
@@ -1,23 +1,75 @@
 <template>
-    <div class="patient-screen">
-      <h1>Patient: {{ patient.username }}</h1>
-      <!-- Display patient information here -->
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        patient: {}
-      };
-    },
-    async created() {
-      const patientId = this.$route.params.id;
-      const response = await fetch('http://localhost:5000/patient/' + patientId);
-      const data = await response.json();
-      this.patient = data;
+  <div class="patient-screen">
+    <!-- NavBar -->
+    <nav class="navbar">
+      <ul>
+        <li><router-link to="/user-details">User Details</router-link></li>
+        <li><router-link to="/upload">Upload File</router-link></li>
+        <li><a href="#" @click="logout">Log Out</a></li>
+      </ul>
+    </nav>
+
+    <!-- תוכן הדף -->
+    <h1>Patient: {{ patient.username }}</h1>
+    <!-- הצגת מידע נוסף על המטופל -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      patient: {}
+    };
+  },
+  async created() {
+    const patientId = this.$route.params.id;
+    const response = await fetch('http://localhost:5000/patient/' + patientId);
+    const data = await response.json();
+    this.patient = data;
+  },
+  methods: {
+    logout() {
+      // הוספת לוגיקה להתנתקות (למשל מחיקת טוקן ואיתחול דף)
+      alert("Logged out successfully!");
+      this.$router.push('/login');
     }
-  };
-  </script>
-  
+  }
+};
+</script>
+
+<style scoped>
+/* עיצוב ה-NavBar */
+.navbar {
+  background-color: #2c3e50;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+.navbar ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+}
+
+.navbar ul li {
+  margin: 0 15px;
+}
+
+.navbar ul li a {
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+}
+
+.navbar ul li a:hover {
+  color: #f39c12;
+}
+
+.patient-screen {
+  text-align: center;
+  padding: 20px;
+}
+</style>
